2e61a37bc38188f0138b0fa25b8355e49c2462e0
Elon Arkell <153341106+ElonArkel@users.noreply.github.com>
2024-10-11 21:52:13 -0300

    Update TestMainRectangle.java

--- Rectangle/TestMainRectangle.java
+++ Rectangle/TestMainRectangle.java
@@ -1,13 +1,13 @@
 public class TestMainRectangle {
-    public static void main(String[] args) { // Testar construtores e toString() // Você precisa anexar um 'f' ou 'F' a um literal float
+    public static void main(String[] args) { 
         Rectangle r1 = new Rectangle(1.2f, 3.4f);
-        System.out.println(r1);   // toString()
-        Rectangle r2 = new Rectangle();   // construtor padrão
-        System.out.println(r2); // Testar setters e getters
+        System.out.println(r1);   
+        Rectangle r2 = new Rectangle();
+        System.out.println(r2);
         r1.setLength(5.6f);
         r1.setWidth(7.8f);
-        System.out.println(r1);   // toString()
-        System.out.println("length is: " + r1.getLength()); System.out.println("width is: " + r1.getWidth()); // Teste getArea() e getPerimeter()
+        System.out.println(r1);  
+        System.out.println("length is: " + r1.getLength()); System.out.println("width is: " + r1.getWidth());
         System.out.printf("a área é: %.2f%n", r1.getArea());
         System.out.printf("o perímetro é: %.2f%n", r1.getPerimeter());
     }
